982a1a8c22842b8dfe6c616e476d196c77490c18
Vitor Mattos <vitor.mattos@caelum.com.br>
2013-12-22 20:23:35 -0200

    Adding multi-level objects test

--- server-test.js
+++ server-test.js
@@ -8,8 +8,16 @@ extend(obj1, obj2);
 console.log(obj1);
 
 var obj3 = { age: 20 };
-var obj4 = { gender: 'male' };
+var obj4 = { 
+	nestedObject: {
+		gender: 'male',
+
+		thirdLevelObject: {
+			value: 300
+		}
+	}
+};
 
 extend(obj1, obj3, obj4);
 
-console.log(obj1);
+console.dir(obj1);
